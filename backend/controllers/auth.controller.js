e9182e6e5deca197ce6bc1e3263bdda3e9b0e805
Guilherme <guilherme.goncalves01@hotmail.com>
2026-03-14 16:24:33 -0300

    Controller de autenticacao criado

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Rota de cadastro chamada!");
+};
+
+export const login = async (req, res) => {
+  res.send("Rota de login chamada!");
+};
+
+export const logout = async (req, res) => {
+  res.send("Rota de logout chamada!");
+};
