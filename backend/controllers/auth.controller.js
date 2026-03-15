51359deb8c757af2b602e61d422b0350fdbe494b
Guilherme <guilherme.goncalves01@hotmail.com>
2026-03-15 09:40:12 -0300

    Adicionado tokens no signup, armazenado eles no redis e criado cookies

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,46 @@
 // definindo as funções de controle para as rotas de autenticação
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import { redis } from "../config/redis.js";
+
+//criando os tokens
+const generateToken = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN, {
+    expiresIn: "15m",
+  });
+
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+//guardando o refreshToken no redis
+const restoreRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refreshToken:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60,
+  );
+};
+
+//setando os cookies
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 15 * 60 * 1000,
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 1000,
+  });
+};
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -13,7 +54,21 @@ export const signup = async (req, res) => {
 
     const user = await User.create({ name, email, password });
 
-    res.status(201).json({ message: "Usuário criado com sucesso", user });
+    //autenticacao
+    const { accessToken, refreshToken } = generateToken(user._id);
+    await restoreRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "Usuário criado com sucesso",
+    });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
