a09cadee7366a5e81d266369fb66a50c2a741b51
Guilherme <guilherme.goncalves01@hotmail.com>
2026-04-14 23:09:06 -0300

    Criado o validator dos cupons e adicionado rota de adicionar o cupom

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getAllCoupons, getCouponById } from "../controllers/coupon.controller.js";
-import { validateParams } from "../middleware/validation.middleware.js";
-import { couponIdParamsSchema } from "../validators/coupon.validator.js";
+import { getAllCoupons, getCouponById, createCoupon } from "../controllers/coupon.controller.js";
+import { validateParams, validateBody } from "../middleware/validation.middleware.js";
+import { couponIdParamsSchema, createCouponSchema } from "../validators/coupon.validator.js";
+import { isAdmin, isLoggedIn } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.get("/", getAllCoupons);
 router.get("/:id", validateParams(couponIdParamsSchema), getCouponById);
+router.post("/", isLoggedIn, isAdmin, validateBody(createCouponSchema), createCoupon);
 
 export default router;
